bf52c482755c86c6cba57dc2e38026f0f0ccd7a8
yUnoming <103978413+yUnoming@users.noreply.github.com>
2024-03-07 12:47:07 +0900

    Fix: release時、ウィンドウのみ生成されるように変更
    【内容】
    releaseビルド時はコンソール画面は不要なので、ウィンドウアプリケーションに変更
    それに伴いエントリポイントのためにmain.cppにwinmain関数を追加

--- C+lan/C+lan/Main.cpp
+++ C+lan/C+lan/Main.cpp
@@ -4,6 +4,7 @@
 #include "Application.h"
 #include <time.h>
 
+#if _DEBUG
 int main()
 {
     srand((unsigned int)time(NULL));    // 乱数用
@@ -14,3 +15,15 @@ int main()
 
     return 0;
 }
+#else
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    srand((unsigned int)time(NULL));    // 乱数用
+
+    //**  アプリケーション実行  **//
+    Ctlan::PrivateSystem::Application app;
+    app.Run();
+
+    return 0;
+}
+#endif
